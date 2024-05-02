201f921043b803b462334c86f8678b8fb2d0ba78
Tomer Baum <tbaum@proofpoint.com>
2024-05-02 17:52:03 +0300

    add other small code to bitbucket update run.sh file add zips/a.txt file plus add 1 files

--- bash/run.sh
+++ bash/run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-for var in "$@"
-do
-    echo "$var"
-done
+#for var in "$@"
+#do
+ #   echo "$var"
+#done
 
+while getopts u:a:f: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        a) age=${OPTARG};;
+        f) fullname=${OPTARG};;
+    esac
+done
+echo "Username: $username";
+echo "Age: $age";
+echo "Full Name: $fullname";
